b722a7a880841e88cff1c321c77177530a3ae1ec
Vincent Verhoeven <verhoevenv@gmail.com>
2016-06-29 19:33:40 +0200

    Ch 2 ex 16
    I miss Scala's for-comprehensions though :(

--- src/ii_collections/_16_FlatMap.kt
+++ src/ii_collections/_16_FlatMap.kt
@@ -7,12 +7,6 @@ fun example() {
     result == listOf('a', 'b', 'c', '1', '2')
 }
 
-val Customer.orderedProducts: Set<Product> get() {
-    // Return all products ordered by customer
-    todoCollectionTask()
-}
+val Customer.orderedProducts: Set<Product> get() = this.orders.flatMap { it.products }.toSet()
 
-val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
-}
+val Shop.allOrderedProducts: Set<Product> get() = this.customers.flatMap { it.orders }.flatMap { it.products }.toSet()
