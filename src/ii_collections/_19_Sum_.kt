436508e0831980cb15bcc99cf00b1bcd66ab08f2
Vincent Verhoeven <verhoevenv@gmail.com>
2016-06-29 20:33:51 +0200

    Ch 2 ex 19
    I really really miss for-comprehensions :/

--- src/ii_collections/_19_Sum_.kt
+++ src/ii_collections/_19_Sum_.kt
@@ -4,8 +4,4 @@ fun example6() {
     val sum = listOf(1, 5, 3).sum()
 }
 
-fun Customer.getTotalOrderPrice(): Double {
-    // Return the sum of prices of all products that a customer has ordered.
-    // Note: a customer may order the same product for several times.
-    todoCollectionTask()
-}
+fun Customer.getTotalOrderPrice(): Double = this.orders.flatMap { it.products }.map { it.price }.sum()
